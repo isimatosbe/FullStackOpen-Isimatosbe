5ae2d6de9192570d6ab9e53acacd11f21329d756
Isidro Matos <isimatosbe@gmail.com>
2022-12-19 21:49:30 +0100

    Exercises 4.13-4.14

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -25,7 +25,8 @@ app.use(express.json())
 app.use(middleware.requestLogger)
 
 app.use('/api/blogs', blogsRouter)
-app.use(middleware.errorHandler)
+
 app.use(middleware.unknownEndpoint)
+//app.use(middleware.errorHandler)
 
 module.exports = app
